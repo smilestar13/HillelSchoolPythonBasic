0397cdb155f93d0c3bb7ff44289cd8dc58bfd8f4
smilestar13 <117439849+smilestar13@users.noreply.github.com>
2022-12-06 01:22:05 +0200

    Add files via upload

---
+++ HW16_Greetings.py
@@ -0,0 +1,13 @@
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+
+
+def say_hi(a, b):
+    c = f"Hi. My name is {a} and I'm {b} years old."
+    return c
+
+
+full_name = say_hi("Frank", 68)
+# full_name = say_hi(name, age)
+
+print(full_name)
