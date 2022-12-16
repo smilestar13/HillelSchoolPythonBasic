014466531f474f0f1ee283240c357c092abdc798
smilestar13 <117439849+smilestar13@users.noreply.github.com>
2022-12-16 03:16:09 +0200

    Update HW23_Dict_popular_word.py

--- HW23_Dict_popular_word.py
+++ HW23_Dict_popular_word.py
@@ -1,13 +1,12 @@
 def popular_words(start_str, finders):
-    start_str = start_str.lower()
+    start_str = start_str.lower().split()
     f_dict = {}
     for el in finders:
-        el = f' {el} '
         if el in start_str:
-            f_dict[el.strip()] = start_str.count(el)
+            f_dict[el] = start_str.count(el)
         else:
-            f_dict[el.strip()] = 0
+            f_dict[el] = 0
     return f_dict
 
 
-print(popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']))
+print(popular_words('''When I was One I had just begun When I was Two I was nearly new''', ['i', 'was', 'three', 'near']))
